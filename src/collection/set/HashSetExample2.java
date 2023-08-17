f1b225bc789f9097aeb3a0b449330360f2549fc4
swimorsink90 <workspacei@naver.com>
2023-08-17 22:07:59 +0900

    컬렉션 예제 : Set

---
+++ src/collection/set/HashSetExample2.java
@@ -0,0 +1,18 @@
+package collection.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample2 {
+	public static void main(String [] args) {
+		Set<Member> set = new HashSet<Member>();
+		
+		set.add(new Member("Å×½ºÆ®1", 30));
+		set.add(new Member("Å×½ºÆ®1", 30));
+		
+		System.out.println("ÃÑ °´Ã¼ ¼ö : "+set.size());
+		
+		
+	}
+
+}
